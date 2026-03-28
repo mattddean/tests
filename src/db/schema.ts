628bb3aa007e5c9f99f961f55b814d25e1885927
Matt Dean <mdean400@gmail.com>
2026-03-27 22:49:56 -0400

    connect better-auth to drizzle

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,9 @@
 import { pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core'
 
-export const todos = pgTable('todos', {
+export const todos = pgTable('todo', {
   id: serial().primaryKey(),
   title: text().notNull(),
   createdAt: timestamp('created_at').defaultNow(),
 })
+
+export * from './auth-schema'
